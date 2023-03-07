714e7a72372f3685e4df7fbc341b822113e35ac3
LemonSauc3 <riley@boyder.ca>
2023-03-07 18:56:07 -0500

    Testing

--- teams/models.py
+++ teams/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Teams(models.Model):
-
+    # Division choices
     division_choices = (("A", "A"), ("B", "B"), ("C", "C"), ("D", "D"))
 
     name = models.CharField(max_length=100, primary_key=True)
